9a12be05632fb585de5be5a7bcdebf489968ac53
Maggie <maggie@xibeis-MacBook.local>
2016-02-04 13:05:53 -0800

    import data

---
+++ HW1/hw1.R
@@ -0,0 +1,3 @@
+# problem 1 Data Analysis and Cleaning
+dataP1 <- read.table("/Users/maggie/Documents/DataMiningCourse/kevinsmall/hw1/data/adult/combined/adult_combined_missing.csv", header = FALSE, sep = ",")
+# (a) Basic Statistical Description
