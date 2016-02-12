489ddf10300a99e11f69ef89f4e19ba554536cf6
Maggie <maggiexu@ccs.neu.edu>
2016-02-12 00:22:42 -0800

    more graphs added from p1

--- HW1/hw1.R
+++ HW1/hw1.R
@@ -126,7 +126,7 @@ printfun <- function(name, col){
 
 # ii)
 ## Calculation:
-cat(
+output <- cat(
 printfun("age", age),
 printfun("fnlwgt", fnlwgt),
 printfun("edu.num", edu.num),
@@ -134,6 +134,18 @@ printfun("cap.gain", cap.gain),
 printfun("cap.loss", cap.loss),
 printfun("hours", hours)
 )
+
+out <- capture.output(cat(
+  printfun("age", age),
+  printfun("fnlwgt", fnlwgt),
+  printfun("edu.num", edu.num),
+  printfun("cap.gain", cap.gain),
+  printfun("cap.loss", cap.loss),
+  printfun("hours", hours)
+))
+
+cat("Output", out, file="/Users/mingzhexu/Documents/MSCS-NEU/2016Spring/DataMining/out.txt", sep="\n")
+
 #####################################
 # (b) Visualizing data
 # setwd("/Users/mingzhexu/Documents/DataMiningCourse/kevinsmall/hw1/data/adult/combined/")
@@ -144,12 +156,31 @@ summary(adult)
 summary(age)
 
 # ii)
-boxplot(age)
-boxplot(age ~ income)
-boxplot(edu.num)
-boxplot(edu.num ~ income)
-boxplot(hours)
-boxplot(hours ~ income)
+boxplot(age, main="age boxplot")
+boxplot(age ~ income, main = "age conditional boxplot")
+#based on the box plot of age, we can derive several arguments:
+# - the median is 37 so half of the people are below 37, half above.
+# - half of the people are from 28 to 48
+# - the conditional boxplot:
+# -- the median age for income <= 50k is less than 37 and median age for income > 50k is more than 40
+
+boxplot(edu.num, main = "education number boxplot")
+boxplot(edu.num ~ income, main = "edu.num conditional boxplot")
+#based on the box plot of education number, we can derive several arguments:
+# - the median is 10 and half of the sample is within 9 - 12
+# - 25% of the sample is 9 and 25% are 11 or 12
+# - median of education number for those income <= 50k is 9, while for income more than 50k, the median education number is obviously more than 10
+
+boxplot(hours, main = "hours boxplot")
+boxplot(hours ~ income, main = "hours conditional boxplot")
+#based on the box plot of hours per month, we can derive several arguments:
+# - the median is 40 and around 25% of the sample is 40
+# - 25% of the sample is between 40 and 45
+# - the conditional boxplot shows that the median hours from people who has income less or equal to 50k is 40, the same as those who earn more than 50k
+# - 75% people whose income is less or equal to 50k work less than or equal to 40 hours
+# - 75% people whose income is more than 50k work more than or equal to 40 hours
+
+# For capital.gain or capital.loss or capital gain, the value is either 0 or a large number and mostly are 0, so we couldn't tell much from the boxplot with median q1 q3 being 0.
 
 # iii)
 hist(age)
@@ -157,13 +188,30 @@ hist(cap.gain)
 hist(cap.loss)
 summary(cap.gain)
 # Interpret
+# The histogram tells more than boxplots.
+# For age, it is a little bit skewed to the left.
+# And there are more people range from age 20 to 45 than other ranges
+# cap.gain and cap.loss have similar characteristics - both are heavily skewed with most values being 0.
 
 # iv)
 wc <- table(adult$workclass)
-barplot(wc)
+barplot(wc, main = "workclass")
 wc2 <- table(income, adult$workclass)
-barplot(wc2, beside=T)
-barplot(prop.table(wc2, 1), beside=T)
+barplot(wc2, beside=T, main = "conditional barplot for workclass")
+barplot(prop.table(wc2, 1), beside=T, main = "conditional barplot for workclass - proportional")
+
+edu <- table(adult$education)
+barplot(edu, main = "education")
+edu2 <- table(income, adult$education)
+barplot(prop.table(edu2, 1), beside=T, main = "conditional barplot for education - proportional")
+
+ms <- table(adult$marital.status)
+ms2 <- table(income, adult$marital.status)
+barplot(ms, main = "marital status")
+barplot(ms2, beside=T, main = "conditional barplot for marital status")
+barplot(prop.table(ms2, 1), beside=T, main = "conditional barplot for marital status - proportional")
+
+ocp <- table(adult$occupation)
 
 # v)
 adult1k <- adult[sample(1:nrow(adult), 100, replace=FALSE),]
